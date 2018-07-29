96d69472e2b3bfe305403f12e33ef90da094d9e7
Matheus Dell <matheushenriquepitz@gmail.com>
2018-07-29 18:33:13 -0300

    Improve the style.

--- client/src/components/ImageDescriptor.js
+++ client/src/components/ImageDescriptor.js
@@ -6,13 +6,11 @@ export const ImageDescriptor = (props) => {
 	let descriptions = props.facesData.map((elem, index) => {
 		return(			
 			<ul key={index} className="collection with-header">
-				<li className="collection-header">Face {index}</li>
-				<li className="collection-header">Gender</li>
-				<li className="collection-item">{elem.gender.gender}</li>
-				<li className="collection-item">{Math.floor(elem.gender.score * 100)}%</li>
-				<li className="collection-header">Age</li>
-				<li className="collection-item">Between {elem.age.min} and {elem.age.max}</li>
-				<li className="collection-item">{Math.floor(elem.age.score * 100)}%</li>
+				<li className="collection-header">Face {index}</li>				
+				<li className="collection-item">Gender: {elem.gender.gender}</li>
+				<li className="collection-item">Probability: {Math.floor(elem.gender.score * 100)}%</li>				
+				<li className="collection-item">Age: between {elem.age.min} and {elem.age.max}</li>
+				<li className="collection-item">Probability: {Math.floor(elem.age.score * 100)}%</li>
 			</ul>							
 		);
 	});
